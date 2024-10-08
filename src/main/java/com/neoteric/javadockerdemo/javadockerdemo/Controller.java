464426adecb1bf08c74ab9c639e7de389bc3f8e6
Likhita <karthavyachowdary1998@gmail.com>
2024-10-08 13:00:34 +0530

    spring boot with jenkins

--- src/main/java/com/neoteric/javadockerdemo/javadockerdemo/Controller.java
+++ src/main/java/com/neoteric/javadockerdemo/javadockerdemo/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/dockerdemo")
     public String dockerdemo(){
-        return "dockerdemo";
+        return "dockerdemo with jenkins";
     }
 
 }
